23e6fc929f88d56ca94dcb047dbfae78f78b1127
guidobenetti9 <95258804+guidobenetti9@users.noreply.github.com>
2024-07-10 23:52:45 -0300

    Inicializacion de proyecto.

---
+++ src/main/java/com/grupo4/LaburApp/LaburAppApplication.java
@@ -0,0 +1,13 @@
+package com.grupo4.LaburApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaburAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaburAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo4/LaburApp/LaburAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo4.LaburApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaburAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
