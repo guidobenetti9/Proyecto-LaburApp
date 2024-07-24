031affa549570687991ef83f1d94cdc8f3f64cd0
Esteban Santillan <negociosebansnt@gmail.com>
2024-07-24 19:45:39 -0300

    User. Mas atributos

--- src/main/java/com/grupo4/LaburApp/models/User.java
+++ src/main/java/com/grupo4/LaburApp/models/User.java
@@ -223,6 +223,38 @@ public class User {
 		this.alternativePhone = alternativePhone;
 	}
 
+	public String getFacebook() {
+		return facebook;
+	}
+
+	public void setFacebook(String facebook) {
+		this.facebook = facebook;
+	}
+
+	public String getTwitter() {
+		return twitter;
+	}
+
+	public void setTwitter(String twitter) {
+		this.twitter = twitter;
+	}
+
+	public String getInstagram() {
+		return instagram;
+	}
+
+	public void setInstagram(String instagram) {
+		this.instagram = instagram;
+	}
+
+	public String getLinkedin() {
+		return linkedin;
+	}
+
+	public void setLinkedin(String linkedin) {
+		this.linkedin = linkedin;
+	}
+
 	public Date getCreatedAt() {
 		return createdAt;
 	}
